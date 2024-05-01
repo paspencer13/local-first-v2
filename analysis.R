e0f5ef005fd6db43cf90f00786df2d7bfdd31289
paspencer13 <rup9@cdc.gov>
2024-05-01 15:42:45 -0400

    Creating a scatter plot

--- analysis.R
+++ analysis.R
@@ -3,4 +3,10 @@ penguins <-read.csv("penguins.csv")
 penguins |> 
   count(island)
 penguins |> 
-  summarize(mean_bill_length = mean(bill_length_mm, na.rm = TRUE))
+  summarize(mean_bill_length = mean(bill_length_mm, na.rm = TRUE)) 
+penguins |> 
+  drop_na(flipper_length_mm, body_mass_g) |> 
+  ggplot(data = penguins,
+         mapping = aes(x = flipper_length_mm,
+                       y = body_mass_g))+
+  geom_point()
